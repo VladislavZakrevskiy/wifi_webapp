b6e193224d2fe4c3d271c7673ed70f2f20b06056
Vladislav-Zakrevskiy <zakrevskiyvlada@yandex.ru>
2024-11-16 19:08:04 +0300

    s

--- src/pages/NotAuth.tsx
+++ src/pages/NotAuth.tsx
@@ -1,6 +1,9 @@
 import { Button, Title, Typography } from "@telegram-apps/telegram-ui";
+import { useNavigate } from "react-router-dom";
 
 export const NotAuth = () => {
+  const nav = useNavigate();
+
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="flex flex-col justify-center items-center p-5 gap-3 ">
@@ -8,7 +11,8 @@ export const NotAuth = () => {
         <Typography>Перейдите в бота @{import.meta.env.VITE_BOT_USERNAME}</Typography>
         <Button
           stretched
-          onClick={() => location.replace(`https://t.me/${import.meta.env.VITE_BOT_USERNAME}`)}
+          // onClick={() => location.replace(`https://t.me/${import.meta.env.VITE_BOT_USERNAME}`)}
+          onClick={() => nav("/ratemarket")}
         >
           Перейти
         </Button>
