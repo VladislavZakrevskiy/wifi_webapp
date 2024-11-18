a2f5e670b72d4123823f30e3cb0c1533a395879c
Vladislav-Zakrevskiy <zakrevskiyvlada@yandex.ru>
2024-11-18 22:22:18 +0300

    showProgress

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -16,7 +16,7 @@ export const PaymentPage = () => {
   useEffect(() => {
     tg.MainButton.text = "Оплатить";
     tg.MainButton.onClick(async () => {
-      tg.MainButton.isProgressVisible = true;
+      tg.MainButton.showProgress();
       await axios.post(
         import.meta.env.VITE_API + "/tg/invoice",
         {
@@ -28,7 +28,6 @@ export const PaymentPage = () => {
           headers: { Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN_KEY)}` },
         }
       );
-      tg.MainButton.isProgressVisible = false;
       tg.close();
     });
     tg.MainButton.show();
